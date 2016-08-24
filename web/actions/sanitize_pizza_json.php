e1e8dbcc484f47941b53472db1f66d6dcabb2800
Khoa Nguyen <khoa@Khoas-MBP-2.domain>
2016-08-23 23:02:34 -0700

    modified json sanitizing script to take into account empties and strings

--- web/actions/sanitize_pizza_json.php
+++ web/actions/sanitize_pizza_json.php
@@ -1,7 +1,15 @@
 <?php 
+if(!isset($_REQUEST) || !isset($_REQUEST["json"]) || empty($_REQUEST["json"])) {
+    header('Content-Type: application/json');
+    echo json_encode([]);
+    die();
+}
+if(!is_array($_REQUEST["json"])) $_REQUEST["json"] = (array) json_decode($_REQUEST["json"], true);
 foreach($_REQUEST["json"] as $i => $o) {
-    if(isset($_REQUEST["json"][$i]["name"])) $_REQUEST["json"][$i]["name"] = strip_tags($o["name"]);
-    if(isset($_REQUEST["json"][$i]["description"])) $_REQUEST["json"][$i]["description"] = strip_tags($o["description"]);
+    if(is_array($_REQUEST["json"])) {
+        if(!empty($_REQUEST["json"][$i]["name"])) $_REQUEST["json"][$i]["name"] = strip_tags($o["name"]);
+        if(!empty($_REQUEST["json"][$i]["description"])) $_REQUEST["json"][$i]["description"] = strip_tags($o["description"]);
+    }
 }
 header('Content-Type: application/json');
 echo json_encode($_REQUEST["json"]);
